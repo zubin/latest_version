803d8668e13587f817be39cba6d3d8b01b624256
Zubin Henner <zubin@users.noreply.github.com>
2020-02-14 23:08:39 +1100

    Cache external requests

--- spec/latest_version_spec.rb
+++ spec/latest_version_spec.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 RSpec.describe LatestVersion do
-  describe '.call' do
+  describe '.call', :vcr do
+    around do |example|
+      VCR.use_cassette(cassette) { example.run }
+    end
+
+    let(:cassette) { self.class.parent_groups.first.name[/[^:]+$/, 0] }
+
     context "ruby" do
       it "returns a valid version" do
         expect(described_class.call('ruby')).to match(/\A\d+\.\d+\.\d+\z/)
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@
 require 'bundler/setup'
 require 'latest_version'
 
+Dir[File.expand_path('support/**/*.rb', __dir__)].each(&method(:require))
+
 RSpec.configure do |config|
   config.disable_monkey_patching!
 
