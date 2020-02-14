6de01c24661ec9d2b96a0612d17238ce9110016a
Zubin Henner <zubin@users.noreply.github.com>
2020-02-14 23:56:03 +1100

    RuboCop

--- lib/latest_version.rb
+++ lib/latest_version.rb
@@ -7,9 +7,18 @@ Dir[File.expand_path('latest_version/**/*.rb', __dir__)].each(&method(:require))
 
 module LatestVersion
   LIBRARIES = {
-    python: -> { Find.call(url: 'https://www.python.org/', regex: %r{<p>Latest: <a href="/downloads/release/python-\d+/">Python (\d+\.\d+\.\d+)</a></p>}) },
-    rails: -> { Find.call(url: 'https://rubyonrails.org/', regex: /Latest version &mdash; Rails ([\d\.]+)/) },
-    ruby: -> { Find.call(url: 'https://www.ruby-lang.org/en/downloads/', regex: /The current stable version is (\d+\.\d+\.\d+)\./) },
+    python: lambda {
+      Find.call(
+        url: 'https://www.python.org/',
+        regex: %r{<p>Latest: <a href="/downloads/release/python-\d+/">Python (\d+\.\d+\.\d+)</a></p>},
+      )
+    },
+    rails: lambda {
+      Find.call(url: 'https://rubyonrails.org/', regex: /Latest version &mdash; Rails ([\d\.]+)/)
+    },
+    ruby: lambda {
+      Find.call(url: 'https://www.ruby-lang.org/en/downloads/', regex: /current stable version is (\d+\.\d+\.\d+)\./)
+    },
   }.freeze
   UnknownLibraryError = Class.new(StandardError)
   private_constant :LIBRARIES
