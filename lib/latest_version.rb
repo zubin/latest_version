84e9a187fadf77dfff467e2298426a4eecde3541
Zubin Henner <zubin@users.noreply.github.com>
2020-02-15 00:00:17 +1100

    Refactor

--- lib/latest_version.rb
+++ lib/latest_version.rb
@@ -1,23 +1,23 @@
 # frozen_string_literal: true
 
+Dir[File.expand_path('latest_version/**/*.rb', __dir__)].each(&method(:require))
+
 require 'net/http'
 require 'uri'
-
-Dir[File.expand_path('latest_version/**/*.rb', __dir__)].each(&method(:require))
 
 module LatestVersion
   LIBRARIES = {
     python: lambda {
-      Find.call(
+      find(
         url: 'https://www.python.org/',
         regex: %r{<p>Latest: <a href="/downloads/release/python-\d+/">Python (\d+\.\d+\.\d+)</a></p>},
       )
     },
     rails: lambda {
-      Find.call(url: 'https://rubyonrails.org/', regex: /Latest version &mdash; Rails ([\d\.]+)/)
+      find(url: 'https://rubyonrails.org/', regex: /Latest version &mdash; Rails ([\d\.]+)/)
     },
     ruby: lambda {
-      Find.call(url: 'https://www.ruby-lang.org/en/downloads/', regex: /current stable version is (\d+\.\d+\.\d+)\./)
+      find(url: 'https://www.ruby-lang.org/en/downloads/', regex: /current stable version is (\d+\.\d+\.\d+)\./)
     },
   }.freeze
   UnknownLibraryError = Class.new(StandardError)
@@ -25,5 +25,9 @@ module LatestVersion
 
   def self.call(library)
     LIBRARIES.fetch(library.to_sym) { raise UnknownLibraryError, library }.call
+  end
+
+  private_class_method def self.find(url:, regex:)
+    Net::HTTP.get(URI.parse(url))[regex, 1]
   end
 end
