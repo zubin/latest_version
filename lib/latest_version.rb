ac98344b9ee5f12151e7b3d29c3f99ed544c6087
Zubin Henner <zubin@users.noreply.github.com>
2020-02-15 05:26:05 +1100

    Get ruby version from Github API

--- lib/latest_version.rb
+++ lib/latest_version.rb
@@ -13,9 +13,7 @@ module LatestVersion
       )
     },
     rails: -> { latest_github_release(repo: 'rails/rails') },
-    ruby: lambda {
-      find_text(url: 'https://www.ruby-lang.org/en/downloads/', regex: /current stable version is (\d+\.\d+\.\d+)\./)
-    },
+    ruby: -> { latest_github_tag(repo: 'ruby/ruby').gsub('_', '.') },
   }.freeze
   UnknownLibraryError = Class.new(StandardError)
   private_constant :LIBRARIES
@@ -35,5 +33,11 @@ module LatestVersion
   private_class_method def self.latest_github_release(repo:)
     json = Net::HTTP.get(URI("https://api.github.com/repos/#{repo}/releases/latest"))
     JSON.parse(json, symbolize_names: true).fetch(:tag_name).gsub(/^v/, '')
+  end
+
+  private_class_method def self.latest_github_tag(repo:)
+    json = Net::HTTP.get(URI("https://api.github.com/repos/#{repo}/tags"))
+    tags = JSON.parse(json, symbolize_names: true)
+    tags.map { |tag| tag.fetch(:name).gsub(/^v/, '') }.reject { |name| name[/beta|preview|rc/] }.max
   end
 end
