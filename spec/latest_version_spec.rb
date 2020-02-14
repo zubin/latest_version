1b6eb393ab10aa22120609c8a0e3384c2bdf8cb3
Zubin Henner <zubin@users.noreply.github.com>
2020-02-15 05:47:16 +1100

    Refactor

--- spec/latest_version_spec.rb
+++ spec/latest_version_spec.rb
@@ -1,35 +1,20 @@
 # frozen_string_literal: true
 
 RSpec.describe LatestVersion do
-  describe '.call', :vcr do
-    around vcr: true do |example|
-      VCR.use_cassette(cassette) { example.run }
-    end
-
-    let(:cassette) { library_name }
-    let(:library_name) { self.class.parent_groups.first.name[/LatestVersion::Call::([^:]+)/, 1].downcase }
-
-    shared_examples "returns a MAJOR.MINOR.PATCH version" do
-      specify do
-        expect(described_class.call(library_name)).to match(/\A\d+\.\d+\.\d+(\.\d+)?\z/)
+  describe '.call' do
+    LatestVersion.supported_libraries.map(&:to_s).each do |library_name|
+      context library_name do
+        it "returns a MAJOR.MINOR.PATCH version" do
+          VCR.use_cassette(library_name) do
+            expect(described_class.call(library_name)).to match(/\A\d+\.\d+\.\d+(\.\d+)?\z/)
+          end
+        end
       end
     end
 
-    context "python" do
-      it_behaves_like "returns a MAJOR.MINOR.PATCH version"
-    end
-
-    context "rails" do
-      it_behaves_like "returns a MAJOR.MINOR.PATCH version"
-    end
-
-    context "ruby" do
-      it_behaves_like "returns a MAJOR.MINOR.PATCH version"
-    end
-
-    context "unknown", vcr: false do
+    context "unknown" do
       it "raises an error" do
-        expect { described_class.call(library_name) }.to raise_error(described_class::UnknownLibraryError, library_name)
+        expect { described_class.call('unknown') }.to raise_error(described_class::UnknownLibraryError, 'unknown')
       end
     end
   end
