5abf8919910f90a2bc3163db3cd0556f5373d498
Zubin Henner <zubin@users.noreply.github.com>
2020-02-14 22:10:51 +1100

    frozen_string_literal

--- lib/latest_version.rb
+++ lib/latest_version.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "latest_version/version"
 
 module LatestVersion
--- spec/latest_version_spec.rb
+++ spec/latest_version_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RSpec.describe LatestVersion do
   it "has a version number" do
     expect(LatestVersion::VERSION).not_to be nil
