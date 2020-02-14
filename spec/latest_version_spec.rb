194ff71035efb3a068a68af7b0a2c4a9b806f2cc
Zubin Henner <zubin@users.noreply.github.com>
2020-02-14 23:20:38 +1100

    Extract shared example

--- spec/latest_version_spec.rb
+++ spec/latest_version_spec.rb
@@ -2,16 +2,21 @@
 
 RSpec.describe LatestVersion do
   describe '.call', :vcr do
-    around do |example|
+    around vcr: true do |example|
       VCR.use_cassette(cassette) { example.run }
     end
 
-    let(:cassette) { self.class.parent_groups.first.name[/[^:]+$/, 0] }
+    let(:cassette) { library_name }
+    let(:library_name) { self.class.parent_groups.first.name[/LatestVersion::Call::([^:]+)/, 1].downcase }
+
+    shared_examples "returns a MAJOR.MINOR.PATCH version" do
+      specify do
+        expect(described_class.call(library_name)).to match(/\A\d+\.\d+\.\d+\z/)
+      end
+    end
 
     context "ruby" do
-      it "returns a valid version" do
-        expect(described_class.call('ruby')).to match(/\A\d+\.\d+\.\d+\z/)
-      end
+      it_behaves_like "returns a MAJOR.MINOR.PATCH version"
     end
   end
 end
