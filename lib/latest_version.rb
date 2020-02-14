34a2fdfb0c0815f7cb8bf1803de7d0e9c35b0487
Zubin Henner <zubin@users.noreply.github.com>
2020-02-14 22:10:22 +1100

    Boilerplate

---
+++ lib/latest_version.rb
@@ -0,0 +1,6 @@
+require "latest_version/version"
+
+module LatestVersion
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/latest_version_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe LatestVersion do
+  it "has a version number" do
+    expect(LatestVersion::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
