22b69fb19298eb9b7407a78dace714cf903a8a20
Zubin Henner <zubin@users.noreply.github.com>
2020-02-15 05:16:54 +1100

    Get rails version from Github API

--- lib/latest_version.rb
+++ lib/latest_version.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'json'
 require 'net/http'
 require 'uri'
 
@@ -11,9 +12,7 @@ module LatestVersion
         regex: %r{<p>Latest: <a href="/downloads/release/python-\d+/">Python (\d+\.\d+\.\d+)</a></p>},
       )
     },
-    rails: lambda {
-      find_text(url: 'https://rubyonrails.org/', regex: /Latest version &mdash; Rails ([\d\.]+)/)
-    },
+    rails: -> { latest_github_release(repo: 'rails/rails') },
     ruby: lambda {
       find_text(url: 'https://www.ruby-lang.org/en/downloads/', regex: /current stable version is (\d+\.\d+\.\d+)\./)
     },
@@ -31,5 +30,10 @@ module LatestVersion
 
   private_class_method def self.find_text(url:, regex:)
     Net::HTTP.get(URI.parse(url))[regex, 1]
+  end
+
+  private_class_method def self.latest_github_release(repo:)
+    json = Net::HTTP.get(URI("https://api.github.com/repos/#{repo}/releases/latest"))
+    JSON.parse(json, symbolize_names: true).fetch(:tag_name).gsub(/^v/, '')
   end
 end
